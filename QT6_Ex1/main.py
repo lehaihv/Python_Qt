ee2959128a06dfdc5c1eab1015d510324a9965f2
Hai LE <haihv@yahoo.com>
2023-07-22 15:57:32 +0700

    Update main.py

--- QT6_Ex1/main.py
+++ QT6_Ex1/main.py
@@ -1,8 +1,14 @@
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QCheckBox, QLabel, QVBoxLayout, QWidget, QLineEdit
+from PyQt6.QtWidgets import (
+    QApplication, QMainWindow, QPushButton, QCheckBox, QLabel,
+    QVBoxLayout, QWidget, QLineEdit, QComboBox
+)
 
 # Only needed for access to command line arguments
 import sys
+
+# Pyserial
+import serial
 
 
 # Subclass QMainWindow to customize your application's main window
@@ -15,7 +21,7 @@ class MainWindow(QMainWindow):
         self.count = count
 
         self.setWindowTitle("Student Management System")
-        self.setFixedSize(QSize(400, 300))  # .setFixedSize() .setMinimumSize() and .setMaximumSize()
+        self.setFixedSize(QSize(800, 600))  # .setFixedSize() .setMinimumSize() and .setMaximumSize()
 
         # Button 1
         self.button = QPushButton("Press Me!")
@@ -30,16 +36,55 @@ class MainWindow(QMainWindow):
         self.button2.clicked.connect(self.the_button_was_clicked)
 
         # label
-        self.label = QLabel("-----")
+        self.label_com_port = QLabel("COM Port")
+        self.label_com_speed = QLabel("Speed (baud)")
+        self.label_com_data = QLabel("Data bits")
+        self.label_com_stop = QLabel("Stop bits")
+        self.label_com_parity = QLabel("Parity")
+        self.label_com_flow = QLabel("Flow control")
 
         # Text
         self.text = QLineEdit("-----")
 
-        # Layout
+        # ComboBox Parity
+        self.combobox_parity = QComboBox()
+        self.combobox_parity.addItems(["None", "Odd", "Even", "Mark", "Space"])
+        # Flag to make the combobox editable
+        # self.combobox_parity.setEditable(True)
+        # Flag to determine how the insert is handled Sends the current index (position) of the selected item
+        # self.combobox_parity.setInsertPolicy(QComboBox.InsertPolicy.InsertAlphabetically)
+        # To limit the number of items allowed in the box
+        # self.combobox_parity.setMaxCount(10)
+        self.combobox_parity.currentIndexChanged.connect(self.parity_index_changed)
+        # There is an alternate signal to send the text.
+        self.combobox_parity.currentTextChanged.connect(self.parity_text_changed)
+
+        # ComboBox Flow control
+        self.combobox_flow = QComboBox()
+        self.combobox_flow.addItems(["None", "XON/XOFF", "RTS/CTS", "DSR/DTR"])
+        # Sends the current index (position) of the selected item.
+        self.combobox_flow.currentIndexChanged.connect(self.parity_index_changed)
+        # There is an alternate signal to send the text.
+        self.combobox_flow.currentTextChanged.connect(self.parity_text_changed)
+
+        # Layout button
         layout = QVBoxLayout()
         layout.addWidget(self.button)
         layout.addWidget(self.button2)
-        layout.addWidget(self.label)
+
+        # Layout label
+        layout.addWidget(self.label_com_port)
+        layout.addWidget(self.label_com_speed)
+        layout.addWidget(self.label_com_data)
+        layout.addWidget(self.label_com_stop)
+        layout.addWidget(self.label_com_parity)
+        layout.addWidget(self.label_com_flow)
+
+        # Layout combobox
+        layout.addWidget(self.combobox_parity)
+        layout.addWidget(self.combobox_flow)
+
+        # Layout text
         layout.addWidget(self.text)
 
         container = QWidget()
@@ -51,8 +96,14 @@ class MainWindow(QMainWindow):
     def the_button_was_clicked(self):
         self.count += 1
         print("Clicked!")
-        self.label.setText("Pressed " + str(self.count) + " times")
-        self.text.setText(self.text.displayText() + " *** " + self.label.text())
+        # self.label.setText("Pressed " + str(self.count) + " times")
+        # self.text.setText(self.text.displayText() + " *** " + self.label.text())
+
+    def parity_index_changed(self, i):  # i is an int
+        print(i)
+
+    def parity_text_changed(self, s):  # s is a str
+        print(s)
 
 
 app = QApplication(sys.argv)
